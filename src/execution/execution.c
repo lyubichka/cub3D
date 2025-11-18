05a37613824b7eedb38fd8bdb1ed7d50be5fdd7f
Hanfas Iqbal <haiqbal@lab3r7s13.42abudhabi.ae>
2025-11-18 17:06:29 +0400

    rendering update

--- src/execution/execution.c
+++ src/execution/execution.c
@@ -6,7 +6,7 @@
 /*   By: haiqbal <haiqbal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/08 14:27:18 by haiqbal           #+#    #+#             */
-/*   Updated: 2025/11/11 20:03:22 by haiqbal          ###   ########.fr       */
+/*   Updated: 2025/11/18 16:46:41 by haiqbal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,28 @@ void	run_engine(t_scene *scene)
 	init_player(&cub.scene);
 	render_scene(&cub);
 	mlx_loop(cub.mlx);
+}
+
+void 	render_scene(t_cub3d *cub)
+{
+	t_image img;
+	int x;
+	t_ray ray;
+
+	init_image(cub->mlx, &img,
+		cub->scene.screen_width,
+		cub->scene.screen_height);
+
+	x = 0;
+	while (x < cub->scene.screen_width)
+	{
+		ft_bzero(&ray, sizeof(t_ray));
+		compute_ray(cub, x, &ray);
+		perform_dda(cub, &ray);
+		compute_wall_height(cub, &ray);
+		draw_vertical_stripe(&img, cub, x, &ray);
+		x++;
+	}
+
+	mlx_put_image_to_window(cub->mlx, cub->win, img.img, 0, 0);
 }
--- src/execution/render/rendering.c
+++ src/execution/render/rendering.c
@@ -6,7 +6,7 @@
 /*   By: haiqbal <haiqbal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/02 19:30:00 by haiqbal           #+#    #+#             */
-/*   Updated: 2025/11/11 20:02:51 by haiqbal          ###   ########.fr       */
+/*   Updated: 2025/11/18 16:46:49 by haiqbal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,16 @@ void compute_ray(t_cub3d *cub, int x, t_ray *ray)
 	cameraX = 2 * x / (double)cub->scene.screen_width - 1;
 	ray->rayDirX = cub->scene.player.dir_x + cub->scene.player.plane_x * cameraX;
 	ray->rayDirY = cub->scene.player.dir_y + cub->scene.player.plane_y * cameraX;
-
 	ray->mapX = (int)cub->scene.player.pos_x;
 	ray->mapY = (int)cub->scene.player.pos_y;
-
-	ray->deltaDistX = fabs(1 / ray->rayDirX);
-	ray->deltaDistY = fabs(1 / ray->rayDirY);
-
-	// initial step
+	if (ray->rayDirX == 0.0)
+	ray->deltaDistX = 1e30; /* effectively infinite */
+	else
+	ray->deltaDistX = fabs(1.0 / ray->rayDirX);
+	if (ray->rayDirY == 0.0)
+	ray->deltaDistY = 1e30;
+	else
+	ray->deltaDistY = fabs(1.0 / ray->rayDirY);
 	if (ray->rayDirX < 0)
 	{
 		ray->stepX = -1;
@@ -37,7 +39,6 @@ void compute_ray(t_cub3d *cub, int x, t_ray *ray)
 		ray->stepX = 1;
 		ray->sideDistX = (ray->mapX + 1.0 - cub->scene.player.pos_x) * ray->deltaDistX;
 	}
-
 	if (ray->rayDirY < 0)
 	{
 		ray->stepY = -1;
@@ -48,7 +49,6 @@ void compute_ray(t_cub3d *cub, int x, t_ray *ray)
 		ray->stepY = 1;
 		ray->sideDistY = (ray->mapY + 1.0 - cub->scene.player.pos_y) * ray->deltaDistY;
 	}
-
 	ray->hit = 0;
 }
 
@@ -77,15 +77,23 @@ void perform_dda(t_cub3d *cub, t_ray *ray)
 void compute_wall_height(t_cub3d *cub, t_ray *ray)
 {
 	if (ray->side == 0)
-		ray->perpWallDist = (ray->mapX - cub->scene.player.pos_x + (1 - ray->stepX) / 2) / ray->rayDirX;
+	{
+		if (ray->rayDirX != 0.0)
+			ray->perpWallDist = (ray->mapX - cub->scene.player.pos_x + (1 - ray->stepX) / 2) / ray->rayDirX;
+		else
+			ray->perpWallDist = 1e30;
+	}
 	else
-		ray->perpWallDist = (ray->mapY - cub->scene.player.pos_y + (1 - ray->stepY) / 2) / ray->rayDirY;
-
+	{
+		if (ray->rayDirY != 0.0)
+			ray->perpWallDist = (ray->mapY - cub->scene.player.pos_y + (1 - ray->stepY) / 2) / ray->rayDirY;
+		else
+			ray->perpWallDist = 1e30;
+	}	
 	ray->lineHeight = (int)(cub->scene.screen_height / ray->perpWallDist);
 	ray->drawStart = -ray->lineHeight / 2 + cub->scene.screen_height / 2;
 	if (ray->drawStart < 0)
 		ray->drawStart = 0;
-
 	ray->drawEnd = ray->lineHeight / 2 + cub->scene.screen_height / 2;
 	if (ray->drawEnd >= cub->scene.screen_height)
 		ray->drawEnd = cub->scene.screen_height - 1;
@@ -94,8 +102,12 @@ void compute_wall_height(t_cub3d *cub, t_ray *ray)
 void draw_vertical_stripe(t_image *img, t_cub3d *cub, int x, t_ray *ray)
 {
 	int y = 0;
-	int color = (ray->side == 1) ? 0xAAAAAA : 0xFFFFFF;
+	int color;
 
+	if(ray->side == 1)
+		color = 0xAAAAAA;
+	else
+		color = 0xFFFFFF;
 	while (y < cub->scene.screen_height)
 	{
 		if (y < ray->drawStart)
@@ -107,30 +119,3 @@ void draw_vertical_stripe(t_image *img, t_cub3d *cub, int x, t_ray *ray)
 		y++;
 	}
 }
-
-void render_scene(t_cub3d *cub)
-{
-	t_image img;
-	int x;
-	t_ray ray;
-
-	init_image(cub->mlx, &img,
-		cub->scene.screen_width,
-		cub->scene.screen_height);
-
-	x = 0;
-	while (x < cub->scene.screen_width)
-	{
-		compute_ray(cub, x, &ray);
-		perform_dda(cub, &ray);
-		compute_wall_height(cub, &ray);
-		draw_vertical_stripe(&img, cub, x, &ray);
-		x++;
-	}
-
-	mlx_put_image_to_window(cub->mlx, cub->win, img.img, 0, 0);
-}
-
-
-
-
