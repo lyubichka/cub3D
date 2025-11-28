8b44146942bc467f6c61bdf04b825cf708502a39
hipman655 <hanfaswb@gmail.com>
2025-11-28 18:41:47 +0400

    done imo

--- include/cub3D.h
+++ include/cub3D.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3D.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: haiqbal <haiqbal@student.42abudhabi.ae>    +#+  +:+       +#+        */
+/*   By: haiqbal <haiqbal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 16:34:41 by veronikalub       #+#    #+#             */
-/*   Updated: 2025/11/26 02:35:03 by haiqbal          ###   ########.fr       */
+/*   Updated: 2025/11/28 18:37:29 by haiqbal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,26 +24,26 @@
 # include <unistd.h>
 
 // constants
-# define MAX_MAP_HEIGHT 100
-# define MAX_MAP_WIDTH 100
-
-// // key codes
-// # define KEY_W      13    /* W */
-// # define KEY_A      0     /* A */
-// # define KEY_S      1     /* S */
-// # define KEY_D      2     /* D */
-// # define KEY_LEFT   123   /* Left arrow */
-// # define KEY_RIGHT  124   /* Right arrow */
-// # define KEY_ESC    53    /* Escape */
+# define MAX_MAP_HEIGHT 1000
+# define MAX_MAP_WIDTH 1000
 
 // key codes
-# define KEY_W 119       /* W */
-# define KEY_A 97        /* A */
-# define KEY_S 115       /* S */
-# define KEY_D 100       /* D */
-# define KEY_LEFT 65363  /* Right arrow */
-# define KEY_RIGHT 65361 /* Left arrow */
-# define KEY_ESC 65307   /* Escape */
+# define KEY_W      13    /* W */
+# define KEY_A      0     /* A */
+# define KEY_S      1     /* S */
+# define KEY_D      2     /* D */
+# define KEY_RIGHT	123   /* Left arrow */
+# define KEY_LEFT	124   /* Right arrow */
+# define KEY_ESC	53    /* Escape */
+
+// key codes
+// # define KEY_W 119       /* W */
+// # define KEY_A 97        /* A */
+// # define KEY_S 115       /* S */
+// # define KEY_D 100       /* D */
+// # define KEY_LEFT 65363  /* Right arrow */
+// # define KEY_RIGHT 65361 /* Left arrow */
+// # define KEY_ESC 65307   /* Escape */
 
 // texture identifiers
 # define TEX_NORTH 0
